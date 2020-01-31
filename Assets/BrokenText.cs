708495fbf390ccf83172fa58c48c64d592a97950
thejusunny <thejusunny@gmail.com>
2020-01-31 18:34:13 +0400

    minor changes

---
+++ Assets/BrokenText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenText : MonoBehaviour
+{
+    private HingeJoint hinge;
+    private void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        Invoke("Break", 3f);
+    }
+    void Break()
+    {
+        hinge.breakForce = 50f;
+        
+    }
+}
