42da81e6937c3adec2288637f562d5a5b55b9a70
thejusunny <thejusunny@gmail.com>
2020-01-31 19:35:43 +0400

    - Working on problem fall

---
+++ Assets/LookAtPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    Camera mainCam;
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+        Debug.Log("Cam: " + mainCam);
+    }
+    void Update()
+    {
+        transform.LookAt(mainCam.transform);
+    }
+}
