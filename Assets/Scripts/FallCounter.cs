66179daf1f3aa88320b9cfa3214e70231cafe762
Aamir Mohammed <darksabre48@gmail.com>
2020-01-31 23:23:47 +0400

    Fixed unusual bugs

--- Assets/Scripts/FallCounter.cs
+++ Assets/Scripts/FallCounter.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FallCounter : MonoBehaviour
 {
-
-    public BrokenText brokenText;
-    public Rigidbody playerRigidbody;
     public ShootText brokenTextShoot;
-    
+
     public int currentBreakCount, maxBreakCount;
-    
+
+    public Rigidbody playerRigidbody;
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) ||
             Input.GetKeyDown(KeyCode.D))
@@ -23,10 +20,8 @@ public class FallCounter : MonoBehaviour
         if (currentBreakCount >= maxBreakCount)
         {
             brokenTextShoot.Shoot();
-            //brokenText.Break();
             playerRigidbody.isKinematic = false;
-       
-            this.enabled = false;
+            enabled = false;
         }
     }
 }
