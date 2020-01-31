d5d2e4edf7c0b81a85da731663dd486be9cc89dd
bornafana <borna.fana@gmail.com>
2020-01-31 17:38:25 +0400

    Checkpoint System
    - Created checkpoint using PlayerPrefs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Checkpoint> checkpoints = new List<Checkpoint>();
+    private PlayerMovement player;
+
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+
+        if (checkpoints.Count <= 0)
+        {
+            checkpoints.AddRange(FindObjectsOfType<Checkpoint>());
+        }
+
+        SpawnPlayer();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    void SpawnPlayer()
+    {
+        string lastPoint = PlayerPrefs.GetString("lastCheckpoint");
+
+        if (lastPoint != null)
+        {
+            Transform spawnPoint = checkpoints.Find(x => x.currentCheckpoint.ToString().Contains(lastPoint)).transform;
+            player.transform.position = spawnPoint.position;
+        }
+    }
+}
